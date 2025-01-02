182ec311218734a184f8f88567f90a4c005fdb35
Ananya <ananya.plainsurf@gmail.com>
2025-01-02 21:09:26 +0530

    First commit

---
+++ src/component/Greet/Greet.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+
+// const Greet = ()=>{
+//     return (
+//         <h1>Hello World</h1>
+//     )
+// }
+
+// export default Greet;
+
+
+import React from "react";
+
+type GreetProps={
+    name:string
+}
+
+
+const Greet:React.FC<GreetProps> = ({name})=>{
+    return (<span>Hello ${name}</span>)
+}
+
+export default Greet
