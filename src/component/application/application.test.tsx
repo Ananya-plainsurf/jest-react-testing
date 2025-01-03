372552f719325b4fb5950419818aa5c3305ada5e
Ananya <ananya.plainsurf@gmail.com>
2025-01-03 13:58:26 +0530

    getByRol : used to grab the element by its default role and test them accordingly

--- src/component/application/application.test.tsx
+++ src/component/application/application.test.tsx
@@ -1,21 +1,32 @@
-import { render,screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 import Application from "./application";
 
+describe("Application Component", () => {
+  test("Application component renders correctly", () => {
+    render(<Application />);
+    const textBoxElement = screen.getByRole("textbox", { name: /name/i });
+    expect(textBoxElement).toBeInTheDocument();
 
-describe("Application Component",()=>{
-    test("Application component renders correctly",()=>{
-        render(<Application />);
-        const textBoxElement =screen.getByRole('textbox');
-        expect(textBoxElement).toBeInTheDocument();
-        
-        const jobLocationElement = screen.getByRole("combobox");
-        expect(jobLocationElement).toBeInTheDocument();
+    const bioElement = screen.getByRole("textbox", { name: /bio/i });
+    expect(bioElement).toBeInTheDocument();
 
-        const termsElement = screen.getByRole('checkbox');
-        expect(termsElement).toBeInTheDocument()
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
 
-        const submitButtonElement = screen.getByRole('button');
-        expect(submitButtonElement).toBeInTheDocument()
-    })
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
 
-})
+    const submitButtonElement = screen.getByRole("button");
+    expect(submitButtonElement).toBeInTheDocument();
+
+    const headingElement = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(headingElement).toBeInTheDocument();
+
+    const headingElement2 = screen.getByRole("heading", {
+      level: 5
+    });
+    expect(headingElement2).toBeInTheDocument();
+  });
+});
--- src/component/application/application.tsx
+++ src/component/application/application.tsx
@@ -1,27 +1,41 @@
-
 const Application = () => {
-    return (
-        <form style={{ display: 'flex', flexDirection: 'column', width: '300px', margin: '0 auto' }}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
+  return (
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "300px",
+        margin: "0 auto",
+      }}
+    >
+        <h1>Application form</h1>
+        <h5>Please fill the form</h5>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+      </div>
+      <div>
+        <label htmlFor="bio">Bio:</label>
+        <textarea id="bio" name="bio" />
+      </div>
 
-            <label htmlFor="jobLocation">Job Location:</label>
-            <select id="jobLocation" name="jobLocation">
-                <option value="">Select a country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="GB">United Kingdom</option>
-                <option value="IN">India</option>
-                <option value="AU">Australia</option>
-            </select>
-            <label>
-                <input type="checkbox" id="terms" name="terms" />
-                I accept the terms and conditions
-            </label>
+      <label htmlFor="jobLocation">Job Location:</label>
+      <select id="jobLocation" name="jobLocation">
+        <option value="">Select a country</option>
+        <option value="US">United States</option>
+        <option value="CA">Canada</option>
+        <option value="GB">United Kingdom</option>
+        <option value="IN">India</option>
+        <option value="AU">Australia</option>
+      </select>
+      <label>
+        <input type="checkbox" id="terms" name="terms" />I accept the terms and
+        conditions
+      </label>
 
-            <button type="submit">Submit</button>
-        </form>
-    );
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default Application;
